5eecddfdd7f8aaeae39b47020f397d9a5e3318fc
Samuel Anozie <samuelanozie10@yahoo.com>
2022-10-10 17:29:25 +0100

    fix: token verification to verify user confirming user is a registered user on database

--- src/middlewares/verifyUser.ts
+++ src/middlewares/verifyUser.ts
@@ -1,5 +1,6 @@
 import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
 import { Request, Response, NextFunction } from 'express';
+import db from '../db/database';
 
 import dotenv from 'dotenv';
 
@@ -17,14 +18,21 @@ export const auth = async (req: Request, res: Response, next: NextFunction) => {
                 .json({ message: 'Invalid Authentication', data: {} });
 
         const decoded = jwt.verify(token, jwtSecret);
-        const { id, name } = decoded as JwtPayload;
-        req.params.logged_user_id = id;
-        req.params.logged_user_name = name;
+        const { id } = decoded as JwtPayload;
+
+        const findUser = await db('users').select('*').where('id', id);
+
+        if (!findUser[0])
+            return res
+                .status(401)
+                .json({ message: 'Invalid Authentication', data: {} });
+
+        req.params.logged_user_id = findUser[0].id;
+        req.params.logged_user_name = findUser[0].name;
     } catch (err) {
         return res
             .status(401)
             .json({ message: 'Invalid Authentication', data: {} });
     }
-
     next();
 };
