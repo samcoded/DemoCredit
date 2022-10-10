06f6d75adeefe27431f91edf388bc174a42c98be
Samuel Anozie <samuelanozie10@yahoo.com>
2022-10-10 16:34:29 +0100

    App server start point

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import route from './routes';
+
+dotenv.config();
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.use('/api', route);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log('Server running on port ' + port);
+    if (process.env.NODE_ENV === 'development') {
+        console.log('http://localhost:' + port);
+    }
+});
